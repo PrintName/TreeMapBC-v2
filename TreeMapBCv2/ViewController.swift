f80d5fee46a19dffbbc4012b2085fb5e42361e73
Allen <54394182+PrintName@users.noreply.github.com>
2020-10-29 18:56:50 -0400

    Initial Commit

---
+++ TreeMapBCv2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TreeMapBCv2
+//
+//  Created by Allen Li on 10/29/20.
+//  Copyright © 2020 Boston College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
